efbd7bc0d7d62e8f7b32d1bb51561f528dbc8dd4
dyrnade <gurescicem@gmail.com>
2017-04-28 11:20:09 +0000

    ADDED dependencies

--- default.nix
+++ default.nix
@@ -1,11 +1,15 @@
-{ mkDerivation, base, process, stdenv }:
+{ mkDerivation, aeson, base, bytestring, directory, filepath, lens
+, process, stdenv, yaml
+}:
 mkDerivation {
   pname = "smallDockerImagesTool";
   version = "0.1.0.0";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
-  executableHaskellDepends = [ base process ];
+  executableHaskellDepends = [
+    aeson base bytestring directory filepath lens process yaml
+  ];
   homepage = "https://github.com/dyrnade/smallDockerImagesTool";
   license = stdenv.lib.licenses.gpl3;
 }
