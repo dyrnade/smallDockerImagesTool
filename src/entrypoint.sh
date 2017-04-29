2bea79655c1484132f86ad137748955725cb5aae
dyrnade <gurescicem@gmail.com>
2017-04-29 14:27:20 +0000

    REMOVED extra fields

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,7 +1,7 @@
 set -e
 # allow the container to be started with `--user`
 if [ "$1" = "redis-server" -a "$(${coreutils}/bin/id -u)" = "0" ]; then
-chown -R redis .
+  chown -R redis .
 fi
 exec "$@"
 
